9dbfbe544c4fb24c24b60ecd89abdf91af73ae25
remzr <110097561+remzr@users.noreply.github.com>
2025-12-26 20:14:58 +0100

    fix header styling

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,8 +2,10 @@ function Header() {
 
   return (
     <header>
-      <h1>babyboard</h1>
-      <p class="time">11:32</p>
+      <div>
+        <h1>babyboard</h1>
+        <p class="time">11:32</p>
+      </div>
     </header>
   )
 }
