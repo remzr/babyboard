7125b8633cfc22514422b28f2e0b43c5ef55e089
remzr <110097561+remzr@users.noreply.github.com>
2025-12-27 15:21:57 +0100

    Starting to setup API request for quote (CORS errors present)

--- src/components/StatsSection.jsx
+++ src/components/StatsSection.jsx
@@ -1,12 +1,39 @@
+import { useState } from "react";
+
 function StatsSection() {
 
-const dailyQuote = "I would have sent a shorter letter, have I had the time."
+const [dailyQuote, setDailyQuote] = useState(null);
+const [dataIsLoaded, setDataIsLoaded] = useState(false);
+const [error, setError] = useState(null);
+
+function newQuote() {
+  fetch("https://stoic.tekloon.net/stoic-quote", {
+    method: "GET",
+    headers: {
+      "Origin": "http://localhost:5173/",
+      "Access-Control-Allow-Origin": "*"
+    }
+  })
+  .then((response) => {
+    if (response.status >= 400) {
+      throw new Error("Server Error");
+    }
+    return response.json();
+  })
+  .then((response) => setDailyQuote(response.quoteText))
+  .catch((error) => setError(error));
+};
+
+function getQuote(e) {
+  setDailyQuote(newQuote);
+}
 
   return (
     <aside id="statsSection">
         <h2>Numerobis</h2>
         <p>12 Weeks</p>
         <p id="dailyQuote">{dailyQuote}</p>
+        <button onClick={getQuote}>New Quote</button>
     </aside>
     )
 }
