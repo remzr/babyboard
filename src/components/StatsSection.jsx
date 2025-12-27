837bcc74db7d4a3005305700f30f9e2d2243512c
remzr <110097561+remzr@users.noreply.github.com>
2025-12-27 14:15:25 +0100

    Dummy text for quote of the day

--- src/components/StatsSection.jsx
+++ src/components/StatsSection.jsx
@@ -1,9 +1,12 @@
 function StatsSection() {
 
+const dailyQuote = "I would have sent a shorter letter, have I had the time."
+
   return (
     <aside id="statsSection">
         <h2>Numerobis</h2>
         <p>12 Weeks</p>
+        <p id="dailyQuote">{dailyQuote}</p>
     </aside>
     )
 }
