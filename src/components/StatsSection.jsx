be360606c2ded9af7531d0350b7ad0813281c8a8
remzr <110097561+remzr@users.noreply.github.com>
2025-12-27 21:59:59 +0100

    Quote-API call working on button click.

--- src/components/StatsSection.jsx
+++ src/components/StatsSection.jsx
@@ -7,33 +7,32 @@ const [dataIsLoaded, setDataIsLoaded] = useState(false);
 const [error, setError] = useState(null);
 
 function newQuote() {
-  fetch("https://stoic.tekloon.net/stoic-quote", {
+  
+  fetch("https://icanhazdadjoke.com/", {
     method: "GET",
-    headers: {
-      "Origin": "http://localhost:5173/",
-      "Access-Control-Allow-Origin": "*"
-    }
-  })
+  headers: {
+    "User-Agent": "babyboard",
+    "Accept": "text/plain"
+  }
+})
   .then((response) => {
     if (response.status >= 400) {
       throw new Error("Server Error");
     }
-    return response.json();
+    return response.text();
   })
-  .then((response) => setDailyQuote(response.quoteText))
+  .then(quoteText => {
+    setDailyQuote(quoteText)
+  })
   .catch((error) => setError(error));
 };
-
-function getQuote(e) {
-  setDailyQuote(newQuote);
-}
 
   return (
     <aside id="statsSection">
         <h2>Numerobis</h2>
         <p>12 Weeks</p>
         <p id="dailyQuote">{dailyQuote}</p>
-        <button onClick={getQuote}>New Quote</button>
+        <button onClick={newQuote}>New Quote</button>
     </aside>
     )
 }
